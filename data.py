052792706596b2b98b7bb35801cbfd26751f9cd0
Bryan Hadro <bryan.e.hadro@gmail.com>
2019-05-23 09:54:42 -0400

    add rest of may cocktail recipes

--- data.py
+++ data.py
@@ -71,7 +71,20 @@ FEASTS = [
     },
     {
         'date': '05-04',
-        'name': 'St. Monica'
+        'name': 'St. Monica',
+        'cocktails': [
+            {
+                'name': 'Merry Widow',
+                'ingredients': [
+                    {'amount': '1.25 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '1.25 oz', 'name': 'Dry Vermouth (Generic)', 'type': 'wine'},
+                    {'amount': '0.25 oz', 'name': 'Bénédictine', 'type': 'liqueur'},
+                    {'amount': '0.5 oz', 'name': 'Absinthe (Generic)', 'type': 'absinthe'},
+                    {'amount': 'dash', 'name': 'Orange Bitters (Generic)', 'type': 'bitters'},
+                    {'amount': '1 twist', 'name': 'Lemon Twist', 'type': 'fruit'}
+                ]
+            }
+        ]
     },
     {
         'date': '05-04',
@@ -79,15 +92,50 @@ FEASTS = [
     },
     {
         'date': '05-04',
-        'name': 'St. Florian'
+        'name': 'St. Florian',
+        'cocktails': [
+            {
+                'name': 'Fireman\'s Sour',
+                'ingredients': [
+                    {'amount': '2.0 oz', 'name': 'Lime Juice (Generic)', 'type': 'juice'},
+                    {'amount': '1 tsp', 'name': 'Sugar (Generic)', 'type': 'sugar'},
+                    {'amount': '0.5 oz', 'name': 'Grenadine (Generic)', 'type': 'syrup'},
+                    {'amount': '2.0 oz', 'name': 'Light Rum (Generic)', 'type': 'rum'},
+                    {'amount': 'top with', 'name': 'Soda Water (Generic)', 'type': 'soda'},
+                    {'amount': '1 wheel', 'name': 'Lime (Generic)', 'type': 'fruit'},
+                    {'amount': '1', 'name': 'Cherry (Generic)', 'type': 'fruit'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-05',
-        'name': 'St. Pius V'
+        'name': 'St. Pius V',
+        'cocktails': [
+            {
+                'name': 'Vesper Martini',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Gordon\'s Gin', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'Vodka (Generic)', 'type': 'vodka'},
+                    {'amount': '0.25 oz', 'name': 'Lillet Blanc', 'type': 'aperitif'},
+                    {'amount': '1 thin peel slice', 'name': 'Lemon (Generic)', 'type': 'fruit'},
+
+                ]
+            }
+        ]
     },
     {
         'date': '05-06',
-        'name': 'St. John at the Latin Gate'
+        'name': 'St. John at the Latin Gate',
+        'cocktails': [
+            {
+                'name': 'Flaming Boilermaker',
+                'ingredients': [
+                    {'amount': '1 pint', 'name': 'Beer (Generic)', 'type': 'beer'},
+                    {'amount': '1.0 oz', 'name': 'Whiskey (Generic)', 'type': 'whiskey'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-07',
@@ -95,11 +143,41 @@ FEASTS = [
     },
     {
         'date': '05-09',
-        'name': 'St. Beatus'
+        'name': 'St. Beatus',
+        'cocktails': [
+            {
+                'name': 'Swiss Cocktail',
+                'ingredients': [
+                    {'amount': '1.25 oz', 'name': 'Dubonnet (Red)', 'type': 'aperitif'},
+                    {'amount': '1.25 oz', 'name': 'Kirsch (Generic)', 'type': 'liquor'},
+                    {'amount': '1 twist', 'name': 'Lemon (Generic)', 'type': 'fruit'},
+                ]
+            },
+            {
+                'name': 'Swiss Alps Cocktail',
+                'ingredients': [
+                    {'amount': '0.75 oz', 'name': 'Vodka (Generic)', 'type': 'vodka'},
+                    {'amount': '0.75 oz', 'name': 'Light Créme de Cacao', 'type': 'liqueur'},
+                    {'amount': '0.75', 'name': 'Cheri-Suisse', 'type': 'chocolate cherry liqueur'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-10',
-        'name': 'St. Damien de Veuster'
+        'name': 'St. Damien de Veuster',
+        'cocktails': [
+            {
+                'name': 'The St. Damien',
+                'ingredients': [
+                    {'amount': '1.0 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.75 oz', 'name': 'Pineapple Juice (Generic)', 'type': 'juice'},
+                    {'amount': '0.25 oz', 'name': 'Grenadine (Generic)', 'type': 'syrup'},
+                    {'amount': '0.25 oz', 'name': 'Lemon Juice (Generic)', 'type': 'juice'},
+                    {'amount': '1 splash', 'name': 'Soda Water (Generic)', 'type': 'soda'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-11',
@@ -111,11 +189,43 @@ FEASTS = [
     },
     {
         'date': '05-13',
-        'name': 'St. John the Silent'
+        'name': 'St. John the Silent',
+        'cocktails': [
+            {
+                'name': 'The Silent Monk',
+                'ingredients': [
+                    {'amount': '1.0 oz', 'name': 'Bénédictine liqueur', 'type': 'liqueur'},
+                    {'amount': '0.25 oz', 'name': 'Cointreau liqueur', 'type': 'liqueur'},
+                    {'amount': '1.0 oz', 'name': 'Light Cream (Generic)', 'type': 'cream'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-15',
-        'name': 'St. Peregrine'
+        'name': 'St. Peregrine',
+        'cocktails': [
+            {
+                'name': 'Campari Aranciata Cocktail',
+                'ingredients': [
+                    {'amount': '2.0 oz', 'name': 'Campari', 'type': 'aperitif'},
+                    {'amount': '3.0 oz', 'name': 'San Pellegrino Aranciata', 'type': 'soda'},
+                    {'amount': '1 slice', 'name': 'Orange (Generic)', 'type': 'fruit'},
+                ]
+            },
+            {
+                'name': 'Tom Collins con Pellegrino',
+                'ingredients': [
+                    {'amount': '2.0 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '1 tsp', 'name': 'Powdered Sugar (Generic)', 'type': 'sugar'},
+                    {'amount': '1.0 oz', 'name': 'Lemon Juice (Generic)', 'type': 'juice'},
+                    {'amount': 'top with', 'name': 'Pellegrino', 'type': 'soda'},
+                    {'amount': '1 wedge', 'name': 'Lemon (Generic)', 'type': 'fruit'},
+                    {'amount': '1', 'name': 'Cherry (Generic)', 'type': 'fruit'},
+                    {'amount': '1 slice', 'name': 'Orange (Generic)', 'type': 'fruit'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-15',
@@ -123,7 +233,23 @@ FEASTS = [
     },
     {
         'date': '05-15',
-        'name': 'The Sevan Apostles of Spain'
+        'name': 'The Sevan Apostles of Spain',
+        'cocktails': [
+            {
+                'name': 'Bloody Bishop',
+                'ingredients': [
+                    {'amount': '1.0 oz', 'name': 'Vodka (Generic)', 'type': 'vodka'},
+                    {'amount': '1.0 oz', 'name': 'Sherry (Generic)', 'type': 'wine'},
+                    {'amount': '3.0 oz', 'name': 'Tomato Juice (Generic)', 'type': 'juice'},
+                    {'amount': '0.5 oz', 'name': 'Lemon Juice (Generic)', 'type': 'juice'},
+                    {'amount': '1 dash', 'name': 'Tobasco Sauce', 'type': 'sauce'},
+                    {'amount': '2 dashes', 'name': 'Worcestershire Sauce (Generic)', 'type': 'sauce'},
+                    {'amount': '1 dash', 'name': 'Salt (Generic)', 'type': 'seasoning'},
+                    {'amount': '1 dash', 'name': 'Pepper (Generic)', 'type': 'seasoning'},
+                    {'amount': '1 stalk', 'name': 'Celery (Generic)', 'type': 'vegetable'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-15',
@@ -131,19 +257,85 @@ FEASTS = [
     },
     {
         'date': '05-16',
-        'name': 'St. Brendan'
+        'name': 'St. Brendan',
+        'cocktails': [
+            {
+                'name': 'St. Brendan\'s Isle',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Vanilla Flavored Vodka (Generic)', 'type': 'vanilla flavored vodka'},
+                    {'amount': '1.0 oz', 'name': 'Coffee Liqueur (Generic)', 'type': 'coffee liqueur'},
+                    {'amount': '0.75 oz', 'name': 'Saint Brendan\'s Irish Cream Liqueur', 'type': 'cream liqueur'},
+                ]
+            },
+            {
+                'name': 'Caramel Truffle Shake',
+                'ingredients': [
+                    {'amount': '1.0 oz', 'name': 'Saint Brendan\'s Irish Cream Liqueur', 'type': 'cream liqueur'},
+                    {'amount': '1.5 oz', 'name': 'Caramel Flavored Vodka (Generic)', 'type': 'caramel flavored vodka'},
+                    {'amount': '2 scoops', 'name': 'Vanilla Ice Cream (Generic)', 'type': 'vanilla ice cream'},
+                    {'amount': '1 tbsp', 'name': 'Creamy Peanut Butter (Generic)', 'type': 'peanut butter'},
+                    {'amount': '1 covering', 'name': 'Chocolate Syrup (Generic)', 'type': 'syrup'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-19',
-        'name': 'St. Dunstan'
+        'name': 'St. Dunstan',
+        'cocktails': [
+            {
+                'name': 'Satan\'s Whiskers (Straight)',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'Dry Vermouth (Generic)', 'type': 'dry vermouth'},
+                    {'amount': '0.5 oz', 'name': 'Sweet Vermouth (Generic)', 'type': 'sweet vermouth'},
+                    {'amount': '0.5 oz', 'name': 'Orange Juice (Generic)', 'type': 'juice'},
+                    {'amount': '0.5 oz', 'name': 'Grand Marnier', 'type': 'orange flavored liqueur'},
+                    {'amount': '1 dash', 'name': 'Orange Bitters (Generic)', 'type': 'orange bitters'},
+                ]
+            },
+            {
+                'name': 'Satan\'s Whiskers (Curled)',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'Dry Vermouth (Generic)', 'type': 'dry vermouth'},
+                    {'amount': '0.5 oz', 'name': 'Sweet Vermouth (Generic)', 'type': 'sweet vermouth'},
+                    {'amount': '0.5 oz', 'name': 'Orange Juice (Generic)', 'type': 'juice'},
+                    {'amount': '0.5 oz', 'name': 'Orange Curaçao', 'type': 'orange flavored liqueur'},
+                    {'amount': '1 dash', 'name': 'Orange Bitters (Generic)', 'type': 'orange bitters'},
+                ]
+            },
+        ]
     },
     {
         'date': '05-22',
-        'name': 'St. Rita'
+        'name': 'St. Rita',
+        'cocktails': [
+            {
+                'name': 'St-Rita Cocktail',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Tequila Blanco (Generic)', 'type': 'tequila blanco'},
+                    {'amount': '0.75 oz', 'name': 'St-Germain', 'type': 'elderflower liqueur'},
+                    {'amount': '0.5 oz', 'name': 'Lime Juice (Generic)', 'type': 'juice'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-24',
-        'name': 'Our Lady Help of Christians'
+        'name': 'Our Lady Help of Christians',
+        'cocktails': [
+            {
+                'name': 'Napoleon Cocktail',
+                'ingredients': [
+                    {'amount': '2.0 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.25 oz', 'name': 'Dubonnet (Red)', 'type': 'aperitif'},
+                    {'amount': '1.0 oz', 'name': 'Orange Curaçao', 'type': 'orange liqueur'},
+                    {'amount': '1 dash', 'name': 'Fernet Branca', 'type': 'bitter herbal liqueur'},
+                    {'amount': '1 twist', 'name': 'Lemon (Generic)', 'type': 'fruit'},
+                ]
+            }
+        ]
     },
     {
         'date': '05-25',
@@ -151,7 +343,27 @@ FEASTS = [
     },
     {
         'date': '05-26',
-        'name': 'St. Philip Neri'
+        'name': 'St. Philip Neri',
+        'cocktails': [
+            {
+                'name': 'Heart Warmer',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Kahlúa', 'type': 'coffee liqueur'},
+                    {'amount': '1.0 oz', 'name': 'Vodka (Generic)', 'type': 'vodka'},
+                    {'amount': '1 tsp', 'name': 'Vanilla Extract (Generic)', 'type': 'flavoring'},
+                    {'amount': '3.0 oz', 'name': 'Milk (Generic)', 'type': 'milk'}
+                ]
+            },
+            {
+                'name': 'Heartburn',
+                'ingredients': [
+                    {'amount': '1.0 oz', 'name': 'Light Rum (Generic)', 'type': 'light rum'},
+                    {'amount': '0.75 oz', 'name': 'Amaretto (Generic)', 'type': 'sweet liqueur'},
+                    {'amount': '0.25 oz', 'name': '151-proof rum (Generic)', 'type': '151-proof rum'},
+                    {'amount': '4-6 oz', 'name': 'Cranberry Juice (Generic)', 'type': 'juice'}
+                ]
+            }
+        ]
     },
     {
         'date': '05-26',
@@ -163,7 +375,27 @@ FEASTS = [
     },
     {
         'date': '05-28',
-        'name': 'St. Germanus'
+        'name': 'St. Germanus',
+        'cocktails': [
+            {
+                'name': 'St-Germain Cocktail',
+                'ingredients': [
+                    {'amount': '2.0 oz', 'name': 'Brut Champagne (Generic)', 'type': 'dry sparkling wine'},
+                    {'amount': '1.5 oz', 'name': 'St-Germain', 'type': 'elderflower liqueur'},
+                    {'amount': '2.0 oz', 'name': 'Club Soda', 'type': 'soda'},
+                    {'amount': '1 twist', 'name': 'Lemon (Generic)', 'type': 'fruit'}
+                ]
+            },
+            {
+                'name': 'St-Germain Martini',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'St-Germain', 'type': 'elderflower liqueur'},
+                    {'amount': '1 twist', 'name': 'Lemon (Generic)', 'type': 'fruit'},
+                    {'amount': 'top with', 'name': 'Dry Sparkling Wine (Generic)', 'type': 'dry sparkling wine'}
+                ]
+            }
+        ]
     },
     {
         'date': '05-30',
@@ -171,10 +403,52 @@ FEASTS = [
     },
     {
         'date': '05-30',
-        'name': 'St. Joan of Arc'
+        'name': 'St. Joan of Arc',
+        'cocktails': [
+            {
+                'name': 'Maiden\'s Blush',
+                'ingredients': [
+                    {'amount': '2.0 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 tsp', 'name': 'Triple Sec (Generic)', 'type': 'orange liqueur'},
+                    {'amount': '0.5 tsp', 'name': 'Grenadine (Generic)', 'type': 'syrup'},
+                    {'amount': '0.5 tsp', 'name': 'Lemon Juice (Generic)', 'type': 'juice'},
+                ]
+            },
+            {
+                'name': 'French 75',
+                'ingredients': [
+                    {'amount': '1.5 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'Lemon Juice (Generic)', 'type': 'juice'},
+                    {'amount': '2.0 tsp', 'name': 'Simple Syrup (Generic)', 'type': 'syrup'},
+                    {'amount': '5.0 oz', 'name': 'Brut Champagne (Generic)', 'type': 'dry sparkling wine'},
+                    {'amount': '1 twist', 'name': 'Lemon (Generic)', 'type': 'fruit'},
+                    {'amount': '1', 'name': 'Cherry (Generic)', 'type': 'fruit'},
+                    {'amount': '1 slice', 'name': 'Orange (Generic)', 'type': 'fruit'}
+                ]
+            }
+        ]
     },
     {
         'date': '05-31',
-        'name': 'Queenship of the Blessed Virgin Mary'
+        'name': 'Queenship of the Blessed Virgin Mary',
+        'cocktails': [
+            {
+                'name': 'Queen',
+                'ingredients': [
+                    {'amount': '1 slice', 'name': 'Pineapple (Generic)', 'type': 'fruit'},
+                    {'amount': '1.75 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'Sweet Vermouth (Generic)', 'type': 'sweet vermouth'},
+                ]
+            },
+            {
+                'name': 'Queen\'s Cocktail',
+                'ingredients': [
+                    {'amount': '1 slice', 'name': 'Pineapple (Generic)', 'type': 'fruit'},
+                    {'amount': '1.5 oz', 'name': 'Gin (Generic)', 'type': 'gin'},
+                    {'amount': '0.5 oz', 'name': 'Sweet Vermouth (Generic)', 'type': 'sweet vermouth'},
+                    {'amount': '0.5 oz', 'name': 'Dry Vermouth (Generic)', 'type': 'dry vermouth'},
+                ]
+            }
+        ]
     }
 ]
